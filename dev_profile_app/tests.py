2d13f88ed11dc99ac5812c00cf8069985ae5ce43
Isah Jacob <isahjacob0@gmail.com>
2023-09-02 21:50:44 +0100

    test.py file

--- dev_profile_app/tests.py
+++ dev_profile_app/tests.py
@@ -1,19 +1,1 @@
-# from django.test import TestCase
-# from django.urls import reverse
-# from rest_framework import status
-# from rest_framework.test import APITestCase
-# from .models import Developer, Project  
-# from .serializers import DeveloperSerializer, ProjectSerializer
-# from django.contrib.auth.models import User
-# from rest_framework.authtoken.models import Token
-# from rest_framework.test import APIClient
-# from rest_framework.test import force_authenticate
 
-# # Create your tests here.
-
-# class DeveloperTests(APITestCase):
-#     """Test the api for developers"""
-#     def setUp(self) -> None:
-#         return super().setUp()
-    
-#     def test_create_developer(self):
