8fdfda33a77d0cf035555d4fecd142926613f605
Isah Jacob <isahjacob0@gmail.com>
2023-09-03 22:55:49 +0100

    added spaces to the end of the file

--- dev_profile_app/urls.py
+++ dev_profile_app/urls.py
@@ -8,12 +8,13 @@ app_name = 'dev_profile_app'
 
 urlpatterns = [
     path('api/register/', RegisterApi.as_view()),
+
     path('api/developers/', DeveloperList.as_view()),
     path('api/developers-detail/<int:developer_id>/', DeveloperDetail.as_view()),
     path('api/developers/create/', DeveloperCreate.as_view()),
     path('api/developers/<int:developer_id>/update/', DeveloperUpdate.as_view()),
     path('api/developers/<int:developer_id>/delete/', DeveloperDelete.as_view()),
-    
+
     path('api/projects/', ProjectList.as_view()),
     path('api/projects-detail/<int:project_id>/', ProjectDetail.as_view()),
     path('api/projects/create/', ProjectCreate.as_view()),
