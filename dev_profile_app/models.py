d55cac1d4d8e0bc5958d6099f16634bf446fb67c
Isah Jacob <isahjacob0@gmail.com>
2023-09-05 17:21:33 +0100

    modified the models

--- dev_profile_app/models.py
+++ dev_profile_app/models.py
@@ -1,6 +1,7 @@
+from django.contrib.auth.models import (AbstractBaseUser, AbstractUser,
+                                        BaseUserManager, Group, Permission,
+                                        PermissionsMixin)
 from django.db import models
-from django.contrib.auth.models import AbstractUser, BaseUserManager
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, Group, Permission
 from django.urls import reverse
 
 # Create your models here.
@@ -29,7 +30,7 @@ class CustomUserManager(BaseUserManager):
         user.is_superuser = True
         user.save(using=self._db)
         return user
-    
+
 
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
@@ -62,20 +63,23 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         return self.email
 
+from django.db import models
+
 
 class Developer(models.Model):
     first_name = models.CharField(max_length=250)
     last_name = models.CharField(max_length=250)
     profile_picture = models.ImageField(upload_to='images/', blank=True)
     technology_stacks = models.ManyToManyField('TechnologyStack')
-    contact_email = models.EmailField()
+    contact_email = models.EmailField(unique=True)
     bio = models.TextField()
     skills = models.CharField(max_length=250)
     github = models.URLField(blank=True)
     linkedin = models.URLField(blank=True)
     twitter = models.URLField(blank=True)
     website = models.URLField(blank=True)
-    
+    projects = models.ManyToManyField('Project')
+
     GENDER_STATUS = (
         ('male', 'Male'),
         ('female', 'Female'),
@@ -85,26 +89,65 @@ class Developer(models.Model):
     gender = models.CharField(max_length=20, choices=GENDER_STATUS, default='male')
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
-    
+        return f"{self.first_name} {self.last_name} - {self.projects}"
+
+
     def get_absolute_url(self):
         return reverse('developer_detail', args=[str(self.id)])
 
 
 
-    
+
 class Project(models.Model):
     title = models.CharField(max_length=250)
     description = models.TextField()
     technologies_used = models.CharField(max_length=250)
     project_url = models.URLField()
-    developer = models.ForeignKey(Developer, on_delete=models.CASCADE)
+    project_image = models.ImageField(upload_to='images/', blank=True)
+    developer_project = models.ManyToManyField('Developer')
 
     def __str__(self):
-        return self.title
+        return f"{self.title} - {', '.join(str(developer) for developer in self.developer_project.all())}"
     
+    def __str__(self):
+        return self.developer_project
+
     def get_absolute_url(self):
         return reverse('project_detail', args=[str(self.id)])
 
 class TechnologyStack(models.Model):
     name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=250)
+    email = models.EmailField()
+    message = models.TextField()
+    subject = models.CharField(max_length=250)
+
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('contact_detail', args=[str(self.id)])
+
+class DeveloperProject(models.Model):
+    developer = models.ForeignKey(Developer, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    role = models.CharField(max_length=250)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    is_currently_working = models.BooleanField(default=False)
+
+
+    def __str__(self):
+        return f"{self.developer}"
+
+
+    
+    def get_absolute_url(self):
+        return reverse('developer_project_detail', args=[str(self.id)])
