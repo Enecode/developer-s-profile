94e26bce5030ec596777151a3ad42f938a9cef99
Isah Jacob <isahjacob0@gmail.com>
2023-09-13 08:46:01 +0100

    modified:   dev_profile_app/models.py to include Experience model

--- dev_profile_app/models.py
+++ dev_profile_app/models.py
@@ -1,15 +1,7 @@
 from django.db import models
 from django.urls import reverse
+from django.core.exceptions import ValidationError
 
-
-class AllData(models.Model):
-    developers = models.ManyToManyField('Developer', blank=True)
-    projects = models.ManyToManyField('Project', blank=True)
-    stacks = models.ManyToManyField('Stack', blank=True)
-    technologies = models.ManyToManyField('Technology', blank=True)
-
-    def __str__(self):
-        return f"{self.developers} {self.projects} {self.stacks} {self.technologies}"
 
 class Developer(models.Model):
     first_name = models.CharField(max_length=250)
@@ -17,6 +9,8 @@ class Developer(models.Model):
     picture = models.ImageField(upload_to='developers/', null=True, blank=True)
     stacks = models.ManyToManyField('Stack', blank=True)
     projects = models.ManyToManyField('Project', blank=True)
+    experiences = models.ManyToManyField('Experience', blank=True)
+    technologies = models.ManyToManyField('Technology', blank=True)
     email = models.EmailField()
     bio = models.TextField()
     skills = models.CharField(max_length=250)
@@ -60,3 +54,32 @@ class Technology(models.Model):
 
     def __str__(self):
         return self.name
+
+class Experience(models.Model):
+    title = models.CharField(max_length=255)
+    currently_working = models.BooleanField(default=False)
+    description = models.TextField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    def validate_experience(self):
+        if self.currently_working:
+            self.end_date = None
+
+        if self.start_date > self.end_date:
+            raise ValidationError("Start date must be before end date.")
+
+
+    def __str__(self):
+        return self.title
+    
+class AllData(models.Model):
+    developers = models.ForeignKey(Developer, on_delete=models.CASCADE)
+    projects = models.ForeignKey(Project, on_delete=models.CASCADE)
+    stacks = models.ForeignKey(Stack, on_delete=models.CASCADE)
+    technologies = models.ForeignKey(Technology, on_delete=models.CASCADE)
+    experience = models.ForeignKey(Experience, on_delete=models.CASCADE)
+    
+
+    def __str__(self):
+        return f"{self.developers} {self.projects} {self.stacks} {self.technologies} {self.experience}"
